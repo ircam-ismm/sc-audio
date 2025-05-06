a74d62b755d34a86d87db492562bfa92a96a5bc5
b-ma <benjamin.matuszewski@gmail.com>
2025-05-06 09:28:06 +0200

    doc: improve homepage

--- docs/src/home.js
+++ docs/src/home.js
@@ -11,115 +11,68 @@ export const template = html`
   <div style="margin-top: 20px">
     <img src="./assets/logo-200x200.png" alt="logo" />
   </div>
-  <!--
-  <p>Simple and robust Web Component library for audio and creative applications in the browser.</p>
-
-  <div id="demo">
-    <sc-bang></sc-bang>
-    <sc-number></sc-number>
-    <sc-slider></sc-slider>
-    <sc-dial></sc-dial>
-    <sc-toggle></sc-toggle>
-    <sc-text>some text</sc-text>
-    <sc-switch></sc-switch>
-  </div>
-
-  <h2 style="margin-top: 60px;">Goal & Philosophy</h2>
 
   <p>
-    This is a rather opinionated library, that primarily aims at providing practical and robust interfaces for practitionners in working and performance situations. Hence, it proposes different interaction modalities such as keyboard and MIDI controls where meaningful, and pretty agressively bypasses or overrides certain default browser's behaviours such as context menus, text selection, etc.
+    <i>sc-audio</i> is a <a href="https://webaudio.github.io/web-audio-api/" target="_blank">Web Audio</a> library for
+    prototyping and creating interactive audio applications both in the browser and Node.js
+    using <a href="isomorphic-web-audio-api">https://github.com/ircam-ismm/isomorphic-web-audio-api</a>].
   </p>
-
-  <h2 style="margin-top: 60px;">Usage</h2>
-
-  <h3>With npm and bundlers</h3>
 
   <p>
-    Install the library using npm (or yarn, or whatever)
+    The main philosophy behind <i>sc-audio</i> is to provide audio nodes that behave similarly to native audio nodes,
+    so that they can be inserted seamlessly into regular Web Audio graphs.
   </p>
 
-  <sc-code-example language="shell">${`
-npm install --save @ircam/sc-components
+  <h2 style="margin-top: 60px;">Install</h2>
+
+  <sc-code-example>${`
+npm install --save @ircam/sc-audio
   `}</sc-code-example>
 
-  <p>Each components lives in its own file and can be imported separately, e.g.:</p>
+  <h2 style="margin-top: 60px;">Example Use</h2>
 
-  <sc-code-example language="javascript">${`
-import { html, render } from 'lit';
-import '@ircam/sc-components/sc-toggle.js';
+  <sc-code-example>${`
+import {
+  AudioContext,
+  AudioBufferSourceNode,
+  ConvolverNode,
+} from 'isomorphic-web-audio-api';
+import {
+  AudioBufferLoader,
+  DistributorNode,
+} from '@ircam/sc-audio';
 
-render(html\`
-  <sc-toggle
-    @change=\${e => console.log(e)}
-  ></sc-toggle>
-\`, document.body);
+// in browsers, you will need to resume on a user gesture
+const audioContext = new AudioContext();
+// load an audio buffer
+const loader = new AudioBufferLoader(audioContext);
+const ir = await loader.load('../assets/room-large.wav');
+const buffer = await loader.load('../assets/drum-loop.wav');
+
+// create the graph
+const convolver = new ConvolverNode(audioContext, { buffer: ir });
+convolver.connect(audioContext.destination);
+
+const dryWet = new DistributorNode(audioContext);
+// connect dry output (0) to destination
+dryWet.connect(audioContext.destination, 0);
+// connect wet output (1) to convolver
+dryWet.connect(convolver, 1);
+
+// pipe a source in the graph
+const src = new AudioBufferSourceNode(audioContext, { buffer, loop: true });
+src.connect(dryWet);
+src.start();
+
+// ramp from dry to wet in 4 seconds, then back to dry
+dryWet.ratio.setValueAtTime(0, audioContext.currentTime);
+dryWet.ratio.linearRampToValueAtTime(1, audioContext.currentTime + buffer.duration);
+dryWet.ratio.exponentialRampToValueAtTime(0.001, audioContext.currentTime + buffer.duration * 2);
   `}</sc-code-example>
 
-  <p>
-    For convenience, we also provide a global entry point which imports all components. However, in most cases you should avoid using this shortcut to keep your bundle size as small as possible:
-  </p>
-
-  <sc-code-example language="javascript">${`
-import '@ircam/sc-components';
-  `}</sc-code-example>
-
-
-  <h3>With unpkg</h3>
-
-  <p>
-    If you don't use a bundler or for simple testing, you can also use the library from [https://unpkg.com/](https://unpkg.com/).
-  </p>
-
-  <h4>In HTML file</h4>
-
-  <sc-code-example language="html">${`
-<!DOCTYPE html>
-<html>
-<head>
-  <script type="module" src="https://unpkg.com/@ircam/sc-components@latest"></script>
-</head>
-<body>
-  <sc-toggle></sc-toggle>
-  <script>
-    const $toggle = document.querySelector('sc-toggle');
-    $toggle.addEventListener('change', e => console.log('Hello toggle', e.detail.value));
-  </script>
-</body>
-<body>
-  `}</sc-code-example>
-
-  <h4>In JS file</h4>
-
-  <sc-code-example language="javascript">${`
-import { html, render } from 'https://unpkg.com/lit-html?module';
-import 'https://unpkg.com/@ircam/sc-components@latest';
-
-render(html\`
-  <sc-toggle
-    @change=\${e => console.log('Hello toggle', e.detail.value)}
-  ></sc-toggle>
-\`, document.body);
-  `}</sc-code-example>
-
-  <p>
-    Note that this method will import the whole bundled library which is quite large. You may not want to use this is production._
-  </p>
-
-  <h3>Integration within other frameworks</h3>
-
-  <p>The library has not been tested within other frameworks such as React or Vue yet.</p>
-  <p>Any feedback is welcome!</p>
-
-  <h3>Credits</h3>
-
-  <p>
-    Vectors and icons by <a href="https://www.svgrepo.com" target="_blank">SVG Repo</a>
-  </p>
 
   <h2 style="margin-top: 60px;">License</h2>
-
-  <a target="_blank" href="https://github.com/ircam-ismm/sc-components/blob/main/LICENSE">BSD-3-Clause</a>
+  <a target="_blank" href="https://github.com/ircam-ismm/sc-audio/blob/main/LICENSE">BSD-3-Clause</a>
 
   <div style="height: 120px;"></div>
--->
 `;
