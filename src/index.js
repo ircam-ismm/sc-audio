4a9e4da72316e91d2edc78eb17d4be817557a45a
b-ma <benjamin.matuszewski@gmail.com>
2025-10-28 09:50:18 +0100

    feat: PhaserNode in progress

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export { CollectorNode } from './CollectorNode.js';
 export { DistributorNode } from './DistributorNode.js';
 export { MuteNode } from './MuteNode.js';
 export { PlaceholderNode } from './PlaceholderNode.js';
+export { PhaserNode } from './PhaserNode.js';
 export { ScaledConstantSourceNode } from './ScaledConstantSourceNode.js';
 export { VolumeNode } from './VolumeNode.js';
 // make more sens here
--- tests/utils.spec.js
+++ tests/utils.spec.js
@@ -1,5 +1,9 @@
 import { assert } from 'chai';
-import { QUARTER_SIN_WAVETABLE, QUARTER_COS_WAVETABLE } from '../src/utils.js';
+import {
+  QUARTER_SIN_WAVETABLE,
+  QUARTER_COS_WAVETABLE,
+  computeCurve,
+} from '../src/utils.js';
 
 describe('# utils', () => {
   describe('## QUARTER_SIN_WAVETABLE', () => {
@@ -13,6 +17,28 @@ describe('# utils', () => {
     it('should start at 1 and end at 0', () => {
       assert.approximately(QUARTER_COS_WAVETABLE[0], 1, 1e-12);
       assert.approximately(QUARTER_COS_WAVETABLE[QUARTER_COS_WAVETABLE.length - 1], 0, 1e-12);
+    });
+  });
+
+  describe('## computeCurve', () => {
+    it('identity', () => {
+      const curve = computeCurve((val) => val, 3);
+      assert.deepEqual(curve, new Float32Array([-1, 0, 1]));
+    });
+
+    it('abs', () => {
+      const curve = computeCurve((val) => Math.abs(val), 3);
+      assert.deepEqual(curve, new Float32Array([1, 0, 1]));
+    });
+
+    it('1 / 2', () => {
+      const curve = computeCurve((val) => val / 2, 3);
+      assert.deepEqual(curve, new Float32Array([-0.5, 0, 0.5]));
+    });
+
+    it('1 / x', () => {
+      const curve = computeCurve((val) => val !== 0 ? 1 / val : 0, 5);
+      assert.deepEqual(curve, new Float32Array([-1, -2, 0, 2, 1]));
     });
   });
 });
