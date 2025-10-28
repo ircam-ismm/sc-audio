02a5d75427069f5b2906a29dbc8ccf0e3dce03ca
b-ma <benjamin.matuszewski@gmail.com>
2025-10-28 11:53:20 +0100

    test(VolumeNode): fix min max options test

--- tests/VolumeNode.spec.js
+++ tests/VolumeNode.spec.js
@@ -1,7 +1,13 @@
 import { assert } from 'chai';
-import { OfflineAudioContext, ConstantSourceNode } from 'isomorphic-web-audio-api';
+import {
+  OfflineAudioContext,
+  ConstantSourceNode,
+  AudioBuffer,
+  AudioBufferSourceNode,
+} from 'isomorphic-web-audio-api';
 import { decibelToLinear } from '@ircam/sc-utils';
 import { VolumeNode } from '../src/index.js';
+import { DEFAULT_WAVETABLE_SIZE } from '../src/utils.js';
 
 const audioContextOptions = { length: 256, numberOfChannels: 1, sampleRate: 48000 };
 
@@ -17,26 +23,6 @@ describe('# VolumeNode', () => {
       const audioContext = new OfflineAudioContext(audioContextOptions);
       const node = new VolumeNode(audioContext, { max: 20 });
       assert.equal(node.max, 20);
-    });
-
-    // it('should expose `curve` option', () => {
-    //   const audioContext = new OfflineAudioContext(audioContextOptions);
-    //   const node = new VolumeNode(audioContext, { curve: [1, 2, 3, 4, 5] });
-    //   assert.deepEqual(node.curve, new Float32Array([1, 2, 3, 4, 5]));
-    // });
-
-    it('should compute curve according to given min and max', () => {
-      const min = -6;
-      const max = 0;
-      const audioContext = new OfflineAudioContext(audioContextOptions);
-      const node = new VolumeNode(audioContext, { min, max });
-      const curve = node.curve;
-
-      for (let i = 0; i < curve.length; i++) {
-        const db = i / (curve.length - 1) * (max - min) + min;
-        const lin = decibelToLinear(db);
-        assert.approximately(lin, curve[i], 1e-7);
-      }
     });
   });
 
@@ -61,21 +47,48 @@ describe('# VolumeNode', () => {
       });
     }
 
-    // const curve = [-20, 0, 6];
-    // const curveTestValues = [-20, -10, 0, 3, 6];
+    it('should compute curve and apply gain according to given min and max', async () => {
+      const min = -6;
+      const max = 0;
+      const audioContext = new OfflineAudioContext({
+        ...audioContextOptions,
+        length: DEFAULT_WAVETABLE_SIZE,
+      });
+      const volume = new VolumeNode(audioContext, { min, max });
 
-    // for (let volume of curveTestValues) {
-    //   it.only('should work with curve', async () => {
-    //     const audioContext = new OfflineAudioContext(audioContextOptions);
-    //     const fader = new VolumeNode(audioContext, { curve, volume });
-    //     const source = new ConstantSourceNode(audioContext, { offset: 1 });
-    //     source.connect(fader).connect(audioContext.destination);
-    //     source.start();
+      const source = new ConstantSourceNode(audioContext, { offset: 1 });
 
-    //     const buffer = await audioContext.startRendering();
-    //     const result = buffer.getChannelData(0);
-    //     const expected = new Float32Array(result.length).fill(decibelToLinear(volume));
-    //   });
-    // }
+      const controlChannel = new Float32Array(DEFAULT_WAVETABLE_SIZE);
+      // populate controlChannel with values in dB
+      for (let i = 0; i < controlChannel.length; i++) {
+        const db = i / (controlChannel.length - 1) * (max - min) + min;
+        controlChannel[i] = db;
+      }
+
+      const controlBuffer = new AudioBuffer({
+        length: DEFAULT_WAVETABLE_SIZE,
+        numberOfChannels: 1,
+        sampleRate: audioContext.sampleRate,
+      });
+      controlBuffer.copyToChannel(controlChannel, 0);
+      const control = new AudioBufferSourceNode(audioContext, { buffer: controlBuffer });
+
+      source.connect(volume).connect(audioContext.destination);
+      control.connect(volume.volume);
+
+      source.start();
+      control.start();
+
+      const buffer = await audioContext.startRendering();
+      const result = buffer.getChannelData(0);
+
+      for (let i = 0; i < controlChannel.length; i++) {
+        const db = controlChannel[i];
+        const sample = result[i]
+        const expected = decibelToLinear(db);
+        console.log(db, sample, expected);
+        assert.approximately(sample, expected, 1e-7);
+      }
+    });
   });
 });
